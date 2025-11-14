3517b0abdaec42189e5b2f71217b5a4e00699bc0
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-11-14 19:45:48 +0530

    vector

---
+++ Practice Folder/vector.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int *vector(int m){
+    int *p = new int[m];
+    cout<<"Enter the elements of the array :";
+    for(int i = 0 ; i< m ;i++){
+        cin>>*(p+i);
+    }
+    return p;
+}
+void display(int *p , int m){
+    cout<<"Display the elements of the array :"<<endl;
+    for(int i = 0 ; i< m ;i++){
+        cout<<*(p+i)<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int m;
+    cout<<"Enter the size of the array : ";
+    cin>>m;
+    int *myArray = vector(m);
+    display(myArray , m);
+    delete [] myArray;
+    return 0;
+}
