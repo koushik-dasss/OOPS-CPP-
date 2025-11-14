c83b0e9f4204e95f23cd57733f517c2f3132813f
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-11-15 00:52:04 +0530

    use of default args while calling the func

---
+++ PYQs/Mid sem 2024/Q3_b.cpp
@@ -0,0 +1,16 @@
+// (b) Write a program that defines a function that receives 4 arguments
+// - char, int, float and double in that order. Make 3 calls to this
+// function. In the second call, pass only 3 arguments; the last one
+// should be taken as 6.28 by default. In the third call, pass only 2
+// arguments. In this case the third argument should be taken as 3.14
+// and fourth as 6.28 by default. [2]
+#include<iostream>
+using namespace std;
+void show(char c , int i , float f = 3.14 , double d =6.28){
+    cout<<c<<" "<<i<<" "<<f<<" "<<d<<endl;
+}
+int main(){
+    show('A',2,3.6,4.8);
+    show('B',2,6.7);
+    show('C',6);
+}
