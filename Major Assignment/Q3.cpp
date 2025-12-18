25423936512662a61d01522f4dff5954d7a83fe8
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-12-18 11:27:29 +0530

    Class has array as it's data members , 4 different operations on the arrays

---
+++ Major Assignment/Q3.cpp
@@ -0,0 +1,84 @@
+// Consider a shopping list of items for which a user can place an order with a dealer every month.
+// The list includes details such as code number and price of each item. Different Operations which
+// can be performed are:
+// 1. Adding an item to the list
+// 2. Deleting an item from the list
+// 3. Printing the total value of the order.
+// 4. Display all items on the list.
+// Write a C++ program using class and objects depicting above scenario. Here, arrays are used as
+// member variables in the class.
+#include<iostream>
+using namespace std;
+class ShoppingList{
+    int code[50];
+    float price[50];
+    int count;
+    public:
+    ShoppingList(){
+        count = 0;
+    }
+    void addItem(){
+        cout<<"Enter item code: "<<endl;
+        cin>>code[count];
+        cout<<"Enter item price: "<<endl;
+        cin>>price[count];
+        count++;
+    }
+    void deleteItem(){
+        int delCode;
+        cout<<"Enter item code to delete: "<<endl;
+        cin>>delCode;
+        for(int i = 0 ; i< count ; i++){
+            if(code[i] == delCode){
+                for(int j = i ; j < count-1 ; j++){
+                    code[j] = code[j+1];
+                    price[j] = price[j+1];
+                }
+                count--;
+                cout<<"Item deleted successfully."<<endl;
+                return;
+            }
+        }
+        cout<<"Item not found."<<endl;
+    }
+    void displayItems(){
+        if(count == 0){
+            cout<<"Shopping list is empty."<<endl;
+            return;
+        }
+        for(int i = 0 ; i < count ; i++){
+            cout<<"Code : "<< code[i] << " Price : "<< price[i] <<endl;
+        }
+    }
+    void totalValue(){
+        float total = 0 ;
+        for(int i = 0 ; i < count ; i++){
+            total+=price[i];
+        }
+        cout<< total <<endl;
+    }
+};
+int main(){
+    ShoppingList list;
+    int n;
+    cout<<"Enter number of items to be added: "<<endl;
+    cin>>n;
+    for(int i = 0 ; i<n ; i++){
+        list.addItem();
+    }
+    cout<<"Displaying items: "<<endl;
+    list.displayItems();
+    cout<<"Total value of items before deletion :"<<endl;
+    list.totalValue();
+    int m;
+    cout<<"Enter number of items to be deleted : "<<endl;
+    cin>>m;
+    for(int i = 0 ; i< m ;i++){
+        list.deleteItem();
+    }
+    cout<<"Displaying items after deletion : "<<endl;
+    list.displayItems();
+    cout<<"Total value of items after deletion: "<<endl;
+    list.totalValue();
+    return 0;
+}
